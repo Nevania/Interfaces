e99325ed2e7040b9723878cfaf88d850bffbabac
Nevania <140127237+Nevania@users.noreply.github.com>
2023-11-12 00:21:10 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioInterfaces/PerroGrande.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    internal class PerroGrande : InterfazPerro
+    {
+        public int Alto { get; set; }
+        public int Peso { get; set; }
+        public int Edad { get; set; }
+
+        public void Ladrar()
+        {
+            Console.WriteLine("El perro grande está ladrando.");
+        }
+
+        public void Correr()
+        {
+            Console.WriteLine("El perro grande está corriendo.");
+        }
+
+        public void Trepar()
+        {
+            Console.WriteLine("El perro grande está trepando.");
+        }
+    }
+}
+
---
+++ EjercicioInterfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EjercicioInterfaces;
+
+public class EjecutarPerro
+{
+    public static void Main()
+    {
+        PerroGrande perroGrande = new PerroGrande { Alto = 50, Peso = 50, Edad = 5 };
+        PerroPequeño perroPequeño = new PerroPequeño { Alto = 30, Peso = 5, Edad = 2 };
+
+        perroGrande.Ladrar();
+        perroGrande.Correr();
+        perroGrande.Trepar();
+
+        perroPequeño.Ladrar();
+        perroPequeño.Correr();
+        perroPequeño.Trepar();
+    }
+}
